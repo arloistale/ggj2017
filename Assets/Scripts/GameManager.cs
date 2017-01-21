fb89739f9cdc343985ddfef3f0d7b1b8ee9afaef
Chaojin <jinchaoonly@gmail.com>
2017-01-21 13:05:56 -0500

    clean up background

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -144,34 +144,34 @@ public class GameManager : MonoBehaviour
 	}
 	
 	//Coroutine to move enemies in sequence.
-	IEnumerator MoveEnemies()
-	{
-		//While enemiesMoving is true player is unable to move.
-		enemiesMoving = true;
-		
-		//Wait for turnDelay seconds, defaults to .1 (100 ms).
-		yield return new WaitForSeconds(turnDelay);
-		
-		//If there are no enemies spawned (IE in first level):
-		if (enemies.Count == 0) 
-		{
-			//Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
-			yield return new WaitForSeconds(turnDelay);
-		}
-		
-		//Loop through List of Enemy objects.
-		for (int i = 0; i < enemies.Count; i++)
-		{
-			//Call the MoveEnemy function of Enemy at index i in the enemies List.
-			enemies[i].MoveEnemy ();
-			
-			//Wait for Enemy's moveTime before moving next Enemy, 
-			yield return new WaitForSeconds(enemies[i].moveTime);
-		}
-		//Once Enemies are done moving, set playersTurn to true so player can move.
-		playersTurn = true;
-		
-		//Enemies are done moving, set enemiesMoving to false.
-		enemiesMoving = false;
-	}
-}
+//	IEnumerator MoveEnemies()
+//	{
+//		//While enemiesMoving is true player is unable to move.
+//		enemiesMoving = true;
+//		
+//		//Wait for turnDelay seconds, defaults to .1 (100 ms).
+//		yield return new WaitForSeconds(turnDelay);
+//		
+//		//If there are no enemies spawned (IE in first level):
+//		if (enemies.Count == 0) 
+//		{
+//			//Wait for turnDelay seconds between moves, replaces delay caused by enemies moving when there are none.
+//			yield return new WaitForSeconds(turnDelay);
+//		}
+//		
+//		//Loop through List of Enemy objects.
+//		for (int i = 0; i < enemies.Count; i++)
+//		{
+//			//Call the MoveEnemy function of Enemy at index i in the enemies List.
+//			enemies[i].MoveEnemy ();
+//			
+//			//Wait for Enemy's moveTime before moving next Enemy, 
+//			yield return new WaitForSeconds(enemies[i].moveTime);
+//		}
+//		//Once Enemies are done moving, set playersTurn to true so player can move.
+//		playersTurn = true;
+//		
+//		//Enemies are done moving, set enemiesMoving to false.
+//		enemiesMoving = false;
+//	}
+//}
