3a45f9d34ac3fa18ff88b1584632347f5ad5c769
Chaojin <jinchaoonly@gmail.com>
2017-01-21 11:45:45 -0500

    Cleanup

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -134,16 +134,16 @@ public class BoardManager : MonoBehaviour
 		InitialiseList ();
 		
 		//Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
-		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+		//LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
 		
 		//Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
-		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
+		//LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
 		
 		//Determine number of enemies based on current level number, based on a logarithmic progression
-		int enemyCount = (int)Mathf.Log(level, 2f);
+		//int enemyCount = (int)Mathf.Log(level, 2f);
 		
 		//Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
-		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+		//LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
 		
 		//Instantiate the exit tile in the upper right hand corner of our game board
 		Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
